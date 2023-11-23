a5faf7d729157e894b984ca40198bef5f28fc7ee
Vedant Bakshi <vedantbakshi07@gmail.com>
2023-11-23 12:32:22 +0530

    Update AutoPopulateCustomer.js

--- Client-Scripts/user-event/AutoPopulateCustomer.js
+++ Client-Scripts/user-event/AutoPopulateCustomer.js
@@ -1,46 +1,23 @@
 /**
- * @NApiVersion 2.x
- * @NScriptType UserEventScript
- * @NModuleScope SameAccount
- */
-define(['N/record', 'N/search'],
-    function (record, search) {
-        function beforeSubmit(scriptContext) {
-            // if (scriptContext.type === scriptContext.UserEventType.VIEW) {
-                var currentRecord = scriptContext.newRecord;
-                var customerId = currentRecord.getValue({
-                    fieldId: 'entity',
-                });
+* @NApiVersion 2.x
+* @NScriptType UserEventScript
+*/
+define(['N/record', 'N/search'],  function(record, search){
+    function beforeLoad(scriptContext){
+        var record_object = scriptContext.newRecord;
+        var i_customerId = 1111; 
+        log.debug('customer Id = ', i_customerId);
+        var customer_object = search.lookupFields({type: 'customer', id: i_customerId, columns: ['email', 'phone']});
+        log.debug('email = ', customer_object.email);
+        log.debug('email = ', customer_object.phone);
+        record_object.setValue({fieldId: 'entity', value: 1111 })
+        record_object.setValue({fieldId: 'custbody_bfl_email', value: customer_object.email})
+        record_object.setValue({fieldId: 'custbody_bfl_pno', value: customer_object.phone})
 
-               
-                    var customerLookup = record.load({
-                        type: record.Type.CUSTOMER,
-                        id: customerId
-                    });
+    }
 
-                    var email = customerLookup.getValue({
-                        fieldId: 'custbody_bfl_email'
-                    });
+    return {beforeLoad :beforeLoad}
 
-                    var phone = customerLookup.getValue({
-                        fieldId: 'custbody_bfl_pno'
-                    });
-
-                    currentRecord.setValue({
-                        fieldId: 'custbody_bfl_email',
-                        value: 'email'
-                    });
-
-                    currentRecord.setValue({
-                        fieldId: 'custbody_bfl_pno',
-                        value: 'phone'
-                    });
-                
-            
-			log.debug('customer ' , customerId )
-        }
-
-        return {
-            beforeLoad: beforeLoad
-        };
-    });
+});
+// Script to Autopopulate Customer name , email ,phone in Invoice transaction record.
+// This is deployed with the name AutoPopulateCustomer.js in NetSuite
