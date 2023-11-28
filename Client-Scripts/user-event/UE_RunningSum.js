6940b9a84668861b6a3978b34901aac6613fcde1
Vedant Bakshi <vedantbakshi07@gmail.com>
2023-11-28 14:30:40 +0530

    added running sum

---
+++ Client-Scripts/user-event/UE_RunningSum.js
@@ -0,0 +1,45 @@
+/**
+ * @NApiVersion 2.x
+ * @NScriptType ClientScript
+ */
+define(['N/record', 'N/log', 'N/currentRecord'], 
+    function(record, log, currentRecord) {
+
+        function pageInit(context) {
+           
+        }
+        function fieldChanged(context) {
+            
+            if (context.sublistId === 'item') {
+    
+                var currentRecordObj = currentRecord.get();
+                var lineCount = currentRecordObj.getLineCount({
+                    sublistId: 'item'
+                });
+
+                var runningTotal = 0;
+
+                for (var i = 0; i < lineCount; i++) {
+                    var amount = currentRecordObj.getSublistValue({
+                        sublistId: 'item',
+                        fieldId: 'amount',
+                        line: i
+                    });
+                    runningTotal += amount;
+                }
+		
+                currentRecordObj.setValue({
+                    fieldId: 'custbody_bfl_sum',
+                    value: runningTotal
+                });
+            }
+        }
+
+        return {
+            pageInit: pageInit,
+            fieldChanged: fieldChanged
+        };
+    }
+);
+
+// SCript to calculate running sum of items in custom running sum field
