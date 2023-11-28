a81e165744156273a1adfd7b79c769b5cc6efc39
Vedant Bakshi <vedantbakshi07@gmail.com>
2023-11-28 16:12:52 +0530

    Updated suitelet

--- Client-Scripts/Suitelet/SL_Shedule_BFL_SelectSO_SetMemo_Demo.js
+++ Client-Scripts/Suitelet/SL_Shedule_BFL_SelectSO_SetMemo_Demo.js
@@ -12,27 +12,21 @@ define(['N/currentRecord', 'N/ui/serverWidget', 'N/url', 'N/https', 'N/task', 'N
                 const customerName = scriptContext.request.parameters.custpage_customer_name;
                 const fromDate = scriptContext.request.parameters.custpage_from_date;
                 const toDate = scriptContext.request.parameters.custpage_to_date;
-				const SO_Array = scriptContext.request.parameters.custpage_arr;
+                const sales_order_arr = scriptContext.request.parameters.custpage_arr;
 
-
-                let form = serverWidget.createForm({
+                var form = serverWidget.createForm({
                     title: 'Sales Order'
                 });
-				form.clientScriptFileId =  8801;
-
-
-
+                form.clientScriptFileId = 8801;
                 var primary = form.addFieldGroup({
                     id: 'primary',
                     label: 'Primary Information'
                 });
 
-
                 var secondary = form.addFieldGroup({
                     id: 'secondary',
                     label: 'Duration '
                 });
-
 
                 var customerField = form.addField({
                     id: 'custpage_customer_name',
@@ -41,47 +35,31 @@ define(['N/currentRecord', 'N/ui/serverWidget', 'N/url', 'N/https', 'N/task', 'N
                     source: 'customer',
                     container: 'primary'
                 });
-				
-				if(customerName){
-					customerField.defaultValue = customerName
-				}
 
-                form.addField({
-                    id: 'custpage_location',
-                    type: serverWidget.FieldType.SELECT,
-                    label: 'Location',
-                    source: 'location',
-                    container: 'primary'
-
-                });
-				
-				
-
+                if (customerName) {
+                    customerField.defaultValue = customerName
+                }
                 var fromField = form.addField({
                     id: 'custpage_from_date',
                     type: serverWidget.FieldType.DATE,
                     label: 'From',
                     container: 'secondary'
-
-
                 });
-				
-				if(fromDate){
-					fromField.defaultValue = fromDate
-				}
+
+                if (fromDate) {
+                    fromField.defaultValue = fromDate
+                }
 
                 var toField = form.addField({
                     id: 'custpage_to_date',
                     type: serverWidget.FieldType.DATE,
                     label: 'To',
                     container: 'secondary'
-
-
                 });
-				
-				if(toDate){
-					toField.defaultValue = toDate
-				}
+
+                if (toDate) {
+                    toField.defaultValue = toDate
+                }
 
                 var sales_sublist = form.addSublist({
                     id: 'sales_order_sublist',
@@ -92,119 +70,102 @@ define(['N/currentRecord', 'N/ui/serverWidget', 'N/url', 'N/https', 'N/task', 'N
                 sales_sublist.addField({ id: "custpage_tranid", type: serverWidget.FieldType.TEXT, label: "Sales Order Number" });
                 sales_sublist.addField({ id: "custpage_entity", type: serverWidget.FieldType.TEXT, label: "Customer Name" });
                 sales_sublist.addField({ id: "custpage_trandate", type: serverWidget.FieldType.TEXT, label: "Date" }),
-                sales_sublist.addField({ id: "custpage_amount", type: serverWidget.FieldType.TEXT, label: "Amount" });
+                    sales_sublist.addField({ id: "custpage_amount", type: serverWidget.FieldType.TEXT, label: "Amount" });
                 sales_sublist.addField({ id: "custpage_status", type: serverWidget.FieldType.TEXT, label: "Status" });
 
-				if(customerName && fromDate && toDate ){
-					
-					
-					
-					var salesorderSearchObj = search.create({
-                    type: "salesorder",
-                    filters:
-                        [
-                            ["mainline", "is", true],
-                            "AND",
-                            ["entity", "anyof", customerName],
-                            "AND",
-                            ["trandate", "within", fromDate, toDate]
+                if (customerName && fromDate && toDate) {
+                    var salesorderSearchObj = search.create({
+                        type: "salesorder",
+                        filters:
+                            [
+                                ["mainline", "is", true],
+                                "AND",
+                                ["entity", "anyof", customerName],
+                                "AND",
+                                ["trandate", "within", fromDate, toDate]
 
-                        ],
+                            ],
 
-                    columns:
-                        [
-                            search.createColumn({
-                                name: "salesorder",
-                                sort: search.Sort.ASC,
-                                label: "Sales Order"
-                            }),
-                            search.createColumn({ name: "mainline", label: "*" }),
-							search.createColumn({ name: "internalid", label: "Internal Id" }),
-                            search.createColumn({ name: "tranid", label: "Document Number" }),
-                            search.createColumn({ name: "trandate", label: "Date" }),
-                            search.createColumn({ name: "type", label: "Type" }),
-                            search.createColumn({ name: "entity", label: "Name" }),
-                            search.createColumn({ name: "account", label: "Account" }),
-                            search.createColumn({ name: "amount", label: "Amount" }),
-                            search.createColumn({ name: "status", label: "Status" }),
-                            search.createColumn({ name: "currency", label: "Currency" }),
+                        columns:
+                            [
+                                search.createColumn({
+                                    name: "salesorder",
+                                    sort: search.Sort.ASC,
+                                    label: "Sales Order"
+                                }),
+                                search.createColumn({ name: "mainline", label: "*" }),
+                                search.createColumn({ name: "internalid", label: "Internal Id" }),
+                                search.createColumn({ name: "tranid", label: "Document Number" }),
+                                search.createColumn({ name: "trandate", label: "Date" }),
+                                search.createColumn({ name: "type", label: "Type" }),
+                                search.createColumn({ name: "entity", label: "Name" }),
+                                search.createColumn({ name: "account", label: "Account" }),
+                                search.createColumn({ name: "amount", label: "Amount" }),
+                                search.createColumn({ name: "status", label: "Status" }),
+                                search.createColumn({ name: "currency", label: "Currency" }),
 
-                        ]
-                });
-			
-                // log.debug("salesorderSearchObj result count", searchResultCount);
-				var resultSearch = salesorderSearchObj.run().getRange({start:0,end:100});
-
-
-                resultSearch.forEach(function (result, i) {
-                    sales_sublist.setSublistValue({
-                        id: 'custpage_tranid',
-                        line: i,
-                        value: result.getValue({ name: 'internalid' })
+                            ]
                     });
 
-                    sales_sublist.setSublistValue({
-                        id: 'custpage_trandate',
-                        line: i,
-                        value: result.getValue({ name: 'trandate' })
+                    // log.debug("salesorderSearchObj result count", searchResultCount);
+                    var resultSearch = salesorderSearchObj.run().getRange({ start: 0, end: 100 });
+
+                    resultSearch.forEach(function (result, i) {
+                        sales_sublist.setSublistValue({
+                            id: 'custpage_tranid',
+                            line: i,
+                            value: result.getValue({ name: 'internalid' })
+                        });
+
+                        sales_sublist.setSublistValue({
+                            id: 'custpage_trandate',
+                            line: i,
+                            value: result.getValue({ name: 'trandate' })
+                        });
+
+                        sales_sublist.setSublistValue({
+                            id: 'custpage_entity',
+                            line: i,
+                            value: result.getText({ name: 'entity' })
+                        });
+
+                        sales_sublist.setSublistValue({
+                            id: 'custpage_status',
+                            line: i,
+                            value: result.getValue({ name: 'status' })
+                        });
+
+                        sales_sublist.setSublistValue({
+                            id: 'custpage_amount',
+                            line: i,
+                            value: result.getValue({ name: 'amount' })
+                        });
                     });
-
-                    sales_sublist.setSublistValue({
-                        id: 'custpage_entity',
-                        line: i,
-                        value: result.getText({ name: 'entity' })
-                    });
-
-                    sales_sublist.setSublistValue({
-                        id: 'custpage_status',
-                        line: i,
-                        value: result.getValue({ name: 'status' })
-                    });
-
-                    sales_sublist.setSublistValue({
-                        id: 'custpage_amount',
-                        line: i,
-                        value: result.getValue({ name: 'amount' })
-                    });
-
-                });
-				
-				
-					
-					
-				}
-                
-
-
-
+                }
                 form.addSubmitButton({
-                    label: 'Search Sales Order',
+                    label: 'Search',
                 });
-				
-				form.addButton({
-                id: 'custpage_set_memo_button',
-                label: 'Set Memo',
-                functionName: 'setMemo'
-				});
-				
-				if(SO_Array){
-					log.debug('SO_Array' , SO_Array)
-					var scheduledScript = task.create({
-                         taskType: task.TaskType.SCHEDULED_SCRIPT
-                     });
-                      scheduledScript.scriptId = 'customscript_so_id';
-					  scheduledScript.deploymentId = 'customdeploy1';
-					  scheduledScript.params = {
-							'custscript_so' : SO_Array
-					};
-					  var scriptTaskId = scheduledScript.submit();
-                      log.debug("id", scriptTaskId);
-				}
-                
 
+                form.addButton({
+                    id: 'custpage_set_memo_button',
+                    label: 'Set Memo',
+                    functionName: 'setMemo'
+                });
+
+                if (sales_order_arr) {
+                    log.debug('sales_order_arr', sales_order_arr)
+                    var scheduledScript = task.create({
+                        taskType: task.TaskType.SCHEDULED_SCRIPT
+                    });
+                    scheduledScript.scriptId = 'customscript_so_id';
+                    scheduledScript.deploymentId = 'customdeploy1';
+                    scheduledScript.params = {
+                        'custscript_so': sales_order_arr
+                    };
+                    var scriptTaskId = scheduledScript.submit();
+                    log.debug("id", scriptTaskId);
+                }
                 scriptContext.response.writePage(form);
-
-
             }
             else if (scriptContext.request.method === 'POST') {
 
@@ -212,31 +173,29 @@ define(['N/currentRecord', 'N/ui/serverWidget', 'N/url', 'N/https', 'N/task', 'N
                 const customerName = scriptContext.request.parameters.custpage_customer_name;
                 const fromDate = scriptContext.request.parameters.custpage_from_date;
                 const toDate = scriptContext.request.parameters.custpage_to_date;
-				const so_array = scriptContext.request.parameters.custpage_arr;
+                const sales_order_arr = scriptContext.request.parameters.custpage_arr;
 
                 scriptContext.response.sendRedirect({
                     type: 'SUITELET',
                     identifier: 'customscript808',
-					id: 'customdeploy1',
+                    id: 'customdeploy1',
                     parameters: {
                         custpage_customer_name: customerName,
                         custpage_from_date: fromDate,
                         custpage_to_date: toDate
                     }
                 });
-				if(so_array){
-					scriptContext.response.sendRedirect({
-                    type: 'SUITELET',
-                    identifier: 'customscript808',
-					id: 'customdeploy1',
-                    parameters: {
-                        custpage_arr: so_array,
-                        
-                    }
-					});
-				}
+                if (sales_order_arr) {
+                    scriptContext.response.sendRedirect({
+                        type: 'SUITELET',
+                        identifier: 'customscript808',
+                        id: 'customdeploy1',
+                        parameters: {
+                            custpage_arr: sales_order_arr,
 
-
+                        }
+                    });
+                }
             }
         }
         return {
