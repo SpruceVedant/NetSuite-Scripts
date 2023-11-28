bc98b4e1c53d7a4a3076a8c7eb9ee3fa4399f0d5
Vedant Bakshi <vedantbakshi07@gmail.com>
2023-11-28 13:32:34 +0530

    Added DElete restlet

---
+++ Client-Scripts/RESTlet/Delete_RESTlet.js
@@ -0,0 +1,59 @@
+/**
+ * @NApiVersion 2.x
+ * @NScriptType Restlet
+ */
+define(['N/record', 'N/log'], function (record, log) {
+    return {
+        get: function (parameters) {
+            
+        },
+
+        put: function (data) {
+         
+        },
+
+        delete: function (parameters) {
+            try {
+                log.debug({
+                    title: 'RESTlet Execution Started',
+                    details: 'Processing DELETE request...'
+                });
+
+                var contactId = parameters.contactId;
+
+                //to  Check if contactId is missing in the request parameters
+                if (contactId == undefined || contactId == null) {
+                    throw {
+                        name: 'InvalidRequestError',
+                        message: 'Contact ID is missing in the request parameters.'
+                    };
+                }
+
+                // to Delete the contact record
+                record.delete({
+                    type: record.Type.CONTACT,
+                    id: contactId
+                });
+
+                log.debug({
+                    title: 'Contact Data Deleted',
+                    details: 'Contact ID: ' + contactId
+                });
+
+                log.debug({
+                    title: 'RESTlet Execution Completed',
+                    details: 'Processed DELETE request.'
+                });
+
+                return JSON.stringify({ success: true, message: 'Contact data deleted successfully.' });
+            } catch (e) {
+                log.error({
+                    title: 'Error in RESTlet Execution',
+                    details: e
+                });
+
+                throw e;
+            }
+        }
+    };
+});
