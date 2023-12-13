1ea1896c69c4846990d5bf3aeb59f1c15c5795a6
Vedant Bakshi <vedantbakshi07@gmail.com>
2023-12-13 13:26:04 +0530

    Added User Event script for task@1

---
+++ Client-Scripts/user-event/UE_Button_PO.js
@@ -0,0 +1,63 @@
+/**
+ * @NApiVersion 2.1
+ * @NScriptType UserEventScript
+ */
+define(["N/record", "N/ui/serverWidget", "N/runtime"], /**
+ * @param{record} record
+ */ (record, serverWidget, runtime) => {
+  const beforeLoad = (scriptContext) => {
+    var Record_obj = scriptContext.newRecord;
+    var user = runtime.getCurrentUser().id;
+    var checkBoxValue = Record_obj.getValue({
+      fieldId: "custbody_bfl_dropship",
+    });
+ 
+    var form = scriptContext.form;
+    var user_role = runtime.getCurrentUser().role;
+ 
+    form.clientScriptFileId = 8809;
+ 
+    var sendForApprovalValue = form.addButton({
+      id: "custpage_approvalrequest",
+      label: "Send PO For Approval",
+      functionName: "sendForApproval()",
+    });
+ 
+    if (scriptContext.type === "view" && checkBoxValue === true) {
+      sendForApprovalValue.isHidden = false;
+    } else {
+      sendForApprovalValue.isHidden = true;
+    }
+    if (user_role == 1110) {
+      var approvePOValue = form.addButton({
+        id: "custpage_approve_button",
+        label: "Approve PO",
+        functionName: "approvePO()",
+      });
+ 
+      var rejectPOValue = form.addButton({
+        id: "custpage_reject_button",
+        label: "Reject PO",
+        functionName: "rejectPO()",
+      });
+    }
+  };
+ 
+  const beforeSubmit = (scriptContext) => {
+    var user = runtime.getCurrentUser().id;
+ 
+    var newRecord = scriptContext.newRecord;
+    newRecord.setValue({
+      fieldId: "employee",
+      value: user,
+    });
+  };
+ 
+  const afterSubmit = (scriptContext) => {};
+ 
+  return { 
+  beforeLoad,
+  beforeSubmit,
+  afterSubmit };
+});
+ 
