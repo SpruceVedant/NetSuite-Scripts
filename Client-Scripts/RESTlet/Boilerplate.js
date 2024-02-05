858b518cb266b235547b3823139f0010019337fd
Vedant Bakshi <vedantbakshi07@gmail.com>
2024-02-05 23:23:20 +0530

    Create Boilerplate.js

---
+++ Client-Scripts/RESTlet/Boilerplate.js
@@ -0,0 +1,14 @@
+/**
+ * @NApiVersion 2.x
+ * @NScriptType Restlet
+*/
+define([], function() {
+    return {
+     get : function() {
+      return "Hello World!"
+     }
+    }
+}); 
+   
+// Returns hello world
+// later wil be used for extension
