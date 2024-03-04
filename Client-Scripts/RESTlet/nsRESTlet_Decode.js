f441fbba7515ea64694d6a763de29f40b336f129
Vedant Bakshi <vedantbakshi07@gmail.com>
2024-03-04 13:19:29 +0530

    Decode file contents in NetSuite with base64Content

---
+++ Client-Scripts/RESTlet/nsRESTlet_Decode.js
@@ -0,0 +1,67 @@
+/**
+ * @NApiVersion 2.x
+ * @NScriptType Restlet
+ */
+define(['N/file', 'N/encode', 'N/log'], function (file, encode, log) {
+    function post(context) {
+        log.debug('Received context', JSON.stringify(context));
+        try {
+            var base64Content = context.fileContent;
+            var decodedContent = encode.convert({
+                string: base64Content,
+                inputEncoding: encode.Encoding.BASE_64,
+                outputEncoding: encode.Encoding.UTF_8
+            });
+
+            var folderId = context.folderId;
+            var fileName = context.fileName;
+            var extension = fileName.split('.').pop().toUpperCase();
+            var fileTypeMapping = {
+                'PDF': file.Type.PDF,
+                'JPG': file.Type.JPG,
+                'JPEG': file.Type.JPG,
+                'CSV': file.Type.CSV,
+                'TXT': file.Type.PLAINTEXT,
+                'TEXT': file.Type.PLAINTEXT,
+                'LOG': file.Type.PLAINTEXT
+            };
+            var fileType = fileTypeMapping[extension] || file.Type.PLAINTEXT;
+            log.debug('Creating file', { name: fileName, type: fileType });
+
+            var uploadFile = file.create({
+                name: fileName,
+                fileType: fileType,
+                contents: decodedContent,
+                folder: folderId
+            });
+
+            log.debug('Saving file');
+            var fileId = uploadFile.save();
+
+            var fileRecord = file.load({
+                id: fileId
+            });
+
+            var fileUrl = fileRecord.url;
+
+            log.audit('File uploaded successfully', fileUrl);
+
+            return {
+                success: true,
+                message: 'File uploaded successfully!',
+                fileId: fileId,
+                fileUrl: fileUrl
+            };
+        } catch (e) {
+            log.error('Error in file upload', e.toString());
+            return {
+                success: false,
+                message: e.toString()
+            };
+        }
+    }
+
+    return {
+        post: post
+    };
+});
