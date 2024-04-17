531bf2e5fcd70fd6d1b25386169d8b22eb46e2dd
Vedant Bakshi <vedantbakshi07@gmail.com>
2024-04-17 23:41:23 +0530

    Create WA.js

---
+++ Integration/WA.js
@@ -0,0 +1,35 @@
+// SuiteScript 2.x example to send a WhatsApp message directly
+
+define(['N/https', 'N/log'], function(https, log) {
+
+    function sendWhatsAppMessage() {
+        var url = 'https://graph.facebook.com/v13.0/YOUR_PHONE_NUMBER_ID/messages';
+        var headers = {
+            'Authorization': 'Bearer YOUR_ACCESS_TOKEN',
+            'Content-Type': 'application/json'
+        };
+        var body = {
+            messaging_product: "whatsapp",
+            to: "CUSTOMER_PHONE_NUMBER",
+            type: "text",
+            text: {
+                body: "Hello! This is a message from NetSuite."
+            }
+        };
+
+        try {
+            var response = https.post({
+                url: url,
+                headers: headers,
+                body: JSON.stringify(body)
+            });
+            log.debug('WhatsApp API Response', response.body);
+        } catch (e) {
+            log.error('Error sending WhatsApp message', e.toString());
+        }
+    }
+
+    return {
+        sendWhatsAppMessage: sendWhatsAppMessage
+    };
+});
